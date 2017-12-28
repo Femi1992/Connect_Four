9c0859aa8d5343cc9ab17d71ed85aa54b19c1dac
femi <olufemidokun@hotmail.co.uk>
2017-12-28 20:29:05 +0000

    Game complete

--- ConnectFour.js
+++ ConnectFour.js
@@ -7,25 +7,20 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
-//report win
 function reportWin(rowNum,colNum) {
   console.log("You won starting at this row,col");
   console.log(rowNum);
   console.log(colNum);
-
-
-//change colour
-
+}
+// Change the color of a button
 function changeColor(rowIndex,colIndex,color) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
 }
-
 
 // Report Back to current color of a button
 function returnColor(rowIndex,colIndex) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
-
 
 // Take in column index, returns the bottom row that is still gray
 function checkBottom(colIndex) {
@@ -38,12 +33,10 @@ function checkBottom(colIndex) {
   }
 }
 
-
 // Check to see if 4 inputs are the same color
 function colorMatchCheck(one,two,three,four){
   return (one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined);
 }
-
 
 // Check for Horizontal Wins
 function horizontalWinCheck() {
@@ -93,3 +86,54 @@ function diagonalWinCheck() {
     }
   }
 }
+
+// Game End
+function gameEnd(winningPlayer) {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 7; row++) {
+      $('h3').fadeOut('fast');
+      $('h2').fadeOut('fast');
+      $('h1').text(winningPlayer+" has won! Refresh your browser to play again!").css("fontSize", "50px")
+    }
+  }
+}
+
+// Start with Player One
+var currentPlayer = 1;
+var currentName = playerOne;
+var currentColor = player1Color;
+
+// Start with Player One
+$('h3').text(playerOne+": it is your turn, please pick a column to drop your blue chip.");
+
+$('.board button').on('click',function() {
+
+  // Recognize what column was chosen
+  var col = $(this).closest("td").index();
+
+  // Get back bottom available row to change
+  var bottomAvail = checkBottom(col);
+
+  // Drop the chip in that column at the bottomAvail Row
+  changeColor(bottomAvail,col,currentColor);
+
+  // Check for a win or a tie.
+  if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+    gameEnd(currentName);
+  }
+
+  // If no win or tie, continue to next player
+  currentPlayer = currentPlayer * -1 ;
+
+  // Re-Check who the current Player is.
+  if (currentPlayer === 1) {
+    currentName = playerOne;
+    $('h3').text(currentName+": it is your turn, please pick a column to drop your blue chip.");
+    currentColor = player1Color;
+  }else {
+    currentName = playerTwo
+    $('h3').text(currentName+": it is your turn, please pick a column to drop your red chip.");
+    currentColor = player2Color;
+  }
+
+})
