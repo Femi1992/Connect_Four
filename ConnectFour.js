2ed55b91be25da507af723850ad053e2ef450089
femi <olufemidokun@hotmail.co.uk>
2017-12-28 18:23:14 +0000

    Grid complete now to build javascript file for game

---
+++ ConnectFour.js
@@ -0,0 +1,8 @@
+var playerOne = prompt("Player one enter your name you will be blue")
+var player1Color = 'rgb(86, 151, 255)';
+
+var playerTwo = prompt("Player two enter your name you will be red")
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
